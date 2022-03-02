dbac05b2273468e36546872878c5dd5018629d67
Arnaud Barberon <barberon_a@hotmail.fr>
2022-03-02 14:16:45 +0100

    insertion de l'ancre logout

--- includes/nav.php
+++ includes/nav.php
@@ -4,6 +4,17 @@
             <li><a href="index.php?page=home">Accueil</a></li>
             <li><a href="index.php?page=inscription">Inscription</a></li>
             <li><a href="index.php?page=login">Se connecter</a></li>
+            <?php
+            if(isset($_SESSION['login']) && $_SESSION['login'] === true) 
+            {
+                echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+            }
+            else 
+            {
+                echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+                echo "<li><a href=\"index.php?page=inscription\">Inscription</a></li>";
+            }
+            ?>
             <li><a href="index.php?page=observation">renseigner une observation</a></li>
         </ul>
     </nav>
